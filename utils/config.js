bf9ae04565fdbc0ec790cb70d3497b18ee978ac8
chenyonghong <njxmfp@tpmail.cn>
2022-08-11 18:02:35 +0800

    .

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 // 小程序链接
 const base = 'http://192.168.0.194:8080'
-// const base = 'http://192.168.0.218:8099'
+// const base = 'qxt.cygmgs.com'
 // const base = 'https://qxth5.cygmgs.com'
 const fileUpload = 'http://192.168.0.205:8021'
 export {
