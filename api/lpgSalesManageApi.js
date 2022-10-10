4c60412d08ea67b4a965598f107ccad236972480
hudandan <1357899044@qq.com>
2022-10-10 15:54:14 +0800

    价格转换

--- api/lpgSalesManageApi.js
+++ api/lpgSalesManageApi.js
@@ -833,3 +833,17 @@ export function salesBusinessSaveOrEdit(data = {}, message = '', custom = {}) {
 		}
 	})
 }
+// 价格转换
+export function salesOrderTemplateFindChangePriceGoods(data = {}, message = '', custom = {}) {
+  return request({
+    url: baseURL + '/admin/sales/salesOrderTemplate/findChangePriceGoods ',
+		method: 'post',
+		data,
+		message,
+		custom: {
+			auth: custom?.auth || true, // 是否需要token
+			toast: custom?.toast || true, // 弹框
+			catch: custom?.catch || true // catch返回
+		}
+	})
+}
